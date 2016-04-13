f72c0e725c4dfff78207657401c0adf425ea08f2
Bo-Xuan Fan <coffee0127@gmail.com>
2016-04-13 15:53:30 +0800

    add layout

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -31,16 +31,65 @@ public class HelloServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("<HTML>");
-        out.println("<BODY>");
-        out.println("<h1>Hello Emp List</h1>");
-
         try {
             Class.forName(DBInfo.DRIVER);
             Connection con = DriverManager.getConnection(DBInfo.URL, DBInfo.USERID, DBInfo.PASSWD);
             PreparedStatement pstmt = con.prepareStatement("SELECT * FROM emp2 order by empno");
             ResultSet rs = pstmt.executeQuery();
-            out.println("<table border='1'><tr><th>員工編號</th><th>姓名</th><th>職稱</th><th>雇用日期</th><th>薪水</th><th>加給</th><th>部門編號</th></tr>");
+
+            out.println("<html lang='en'>");
+            out.println("<head>");
+            out.println("    <meta charset='UTF-8' />");
+            out.println("    <title>MVC Sample</title>");
+            out.println("    <link href='resources/css/bootstrap.min.css' rel='stylesheet'>");
+            out.println("    <link href='resources/css/starter-template.css' rel='stylesheet'>");
+            out.println("    <style>");
+            out.println("    .thead-default th {");
+            out.println("        color: #55595c;");
+            out.println("        background-color: #eceeef;");
+            out.println("        text-align: center;");
+            out.println("    }");
+            out.println("    </style>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("    <nav class='navbar navbar-inverse navbar-fixed-top'>");
+            out.println("        <div class='container'>");
+            out.println("            <div class='navbar-header'>");
+            out.println("                <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>");
+            out.println("                    <span class='sr-only'>Toggle navigation</span>");
+            out.println("                    <span class='icon-bar'></span>");
+            out.println("                    <span class='icon-bar'></span>");
+            out.println("                    <span class='icon-bar'></span>");
+            out.println("                </button>");
+            out.println("                <a class='navbar-brand' href='" + request.getContextPath() + "'>MVC Demo</a>");
+            out.println("            </div>");
+            out.println("            <div id='navbar' class='collapse navbar-collapse'>");
+            out.println("                <ul class='nav navbar-nav'>");
+            out.println("                    <li><a href='" + request.getContextPath() + "'>Home</a></li>");
+            out.println("                    <li class='active'><a href='" + request.getContextPath() + "/hello.do'>Without MVC</a></li>");
+            out.println("                    <li><a href='" + request.getContextPath() + "/emp.do'>With MVC</a></li>");
+            out.println("                </ul>");
+            out.println("            </div>");
+            out.println("            <!--/.nav-collapse -->");
+            out.println("        </div>");
+            out.println("    </nav>");
+
+            out.println("    <div class='container'>");
+            out.println("        <div class='starter-template'>");
+            out.println("            <h1>Withou MVC Demo</h1>");
+            out.println("            <table class='table table-hover'>");
+            out.println("                <thead class='thead-default'>");
+            out.println("                    <tr>");
+            out.println("                        <th>員工編號</th>");
+            out.println("                        <th>姓名</th>");
+            out.println("                        <th>職稱</th>");
+            out.println("                        <th>雇用日期</th>");
+            out.println("                        <th>薪水</th>");
+            out.println("                        <th>加給</th>");
+            out.println("                        <th>部門編號</th>");
+            out.println("                    </tr>");
+            out.println("                </thead>");
+
             while (rs.next()) {
                 // empVO 也稱為 Domain objects
                 StringBuilder emp = new StringBuilder("<tr align='center'>");
@@ -54,12 +103,16 @@ public class HelloServlet extends HttpServlet {
                 emp.append("</tr>");
                 out.println(emp.toString());
             }
-            out.print("</table>");
+            out.println("            </table>");
+            out.println("        </div>");
+            out.println("    </div>");
 
+            out.println("    <script src='resources/js/jquery.min.js'></script>");
+            out.println("    <script src='resources/js/bootstrap.min.js'></script>");
+            out.println("</body>");
+            out.println("</html>");
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        out.println("</BODY></HTML>");
     }
 }
