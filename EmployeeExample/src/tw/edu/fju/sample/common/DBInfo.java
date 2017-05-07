bc611e3614e26348a8f363cb6795d260b83bb27a
Bo-Xuan Fan <coffee0127@gmail.com>
2017-05-07 10:31:59 +0800

    資料庫連線資訊Constant

---
+++ EmployeeExample/src/tw/edu/fju/sample/common/DBInfo.java
@@ -0,0 +1,9 @@
+package tw.edu.fju.sample.common;
+
+public final class DBInfo {
+    // TODO 0. 修改 DB 連線資訊
+    public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+    public static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    public static final String USERID = "SCOTT";
+    public static final String PASSWD = "TIGER";
+}
