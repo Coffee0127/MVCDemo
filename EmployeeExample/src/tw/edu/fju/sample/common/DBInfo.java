65283f2363813652398baa6e01e6a441522e3d01
Bo-Xuan Fan <coffee0127@gmail.com>
2017-05-08 23:31:57 +0800

    change TODO list number format

--- EmployeeExample/src/tw/edu/fju/sample/common/DBInfo.java
+++ EmployeeExample/src/tw/edu/fju/sample/common/DBInfo.java
@@ -1,7 +1,7 @@
 package tw.edu.fju.sample.common;
 
 public final class DBInfo {
-    // TODO 0. 修改 DB 連線資訊
+    // TODO 00. 修改 DB 連線資訊
     public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
     public static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
     public static final String USERID = "SCOTT";
