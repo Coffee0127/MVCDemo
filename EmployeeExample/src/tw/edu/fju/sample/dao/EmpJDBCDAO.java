aabdebe0a473823bf88ef7b01b138768bd0f99b6
Bo-Xuan Fan <coffee0127@gmail.com>
2017-05-24 10:15:11 +0800

    fix SQL injection

--- EmployeeExample/src/tw/edu/fju/sample/dao/EmpJDBCDAO.java
+++ EmployeeExample/src/tw/edu/fju/sample/dao/EmpJDBCDAO.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -214,58 +215,6 @@ public class EmpJDBCDAO implements EmpDAO {
 
     @Override
     public List<EmpVO> findAll() {
-        return this.doFindAll(GET_ALL_STMT);
-    }
-
-    @Override
-    public List<EmpVO> findAll(Map<String, String> conditions) {
-        StringBuilder sql = new StringBuilder(GET_ALL_BASE_STMT);
-
-        do {
-            if (conditions == null) {
-                break;
-            }
-
-            for (Entry<String, String> entry : conditions.entrySet()) {
-                if (ValidateUtils.isBlank(entry.getValue())) {
-                    continue;
-                }
-                String condition = transformCondition(entry.getKey(), entry.getValue());
-                if (condition != null) {
-                    sql.append(" and ").append(condition);
-                }
-            }
-        } while (false);
-
-        sql.append(" ORDER BY empno");
-        System.out.println("●●finalSQL = " + sql);
-        return this.doFindAll(sql.toString());
-    }
-
-    private String transformCondition(String columnName, String value) {
-        /*
-         * TODO [進階] 思考 in、between、>=、<=、… 時該怎麼撰寫程式碼
-         * 提示：
-         *   1. ServletRequest.getParameterMap() 方法 回傳 java.util.Map<java.lang.String,java.lang.String[]>
-         *   2. 修改方法參數(如使用 enum、常數…等)，使程式可以判斷使用何種 where operator
-         */
-        switch (columnName) {
-            case "empno":
-            case "sal":
-            case "comm":
-            case "deptno":
-                return columnName + " = " + value;
-            case "ename":
-            case "job":
-                return columnName + " like '%" + value + "%'";
-            case "hiredate":
-                return "to_char(" + columnName + ",'yyyy-mm-dd') = '" + value + "'";
-            default:
-                return null;
-        }
-    }
-
-    private List<EmpVO> doFindAll(String sql) {
         List<EmpVO> list = new ArrayList<>();
         EmpVO empVO = null;
 
@@ -275,7 +224,7 @@ public class EmpJDBCDAO implements EmpDAO {
 
         try {
             con = DriverManager.getConnection(url, userid, passwd);
-            pstmt = con.prepareStatement(sql);
+            pstmt = con.prepareStatement(GET_ALL_STMT);
             rs = pstmt.executeQuery();
 
             while (rs.next()) {
@@ -319,6 +268,128 @@ public class EmpJDBCDAO implements EmpDAO {
             }
         }
         return list;
+    }
+
+    public static void main(String[] args) {
+        EmpDAO dao = new EmpJDBCDAO();
+        Map<String, String> conditions = new HashMap<>();
+        conditions.put("empno", "1 or job like '%S%' ORDER BY empno desc --");
+        for (EmpVO empVO : dao.findAll(conditions)) {
+            System.out.println(empVO);
+        }
+    }
+
+    @Override
+    public List<EmpVO> findAll(Map<String, String> conditions) {
+        String sql = transformSQL(conditions);
+        System.out.println("●●finalSQL = " + sql);
+
+        List<EmpVO> list = new ArrayList<>();
+        EmpVO empVO = null;
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = DriverManager.getConnection(url, userid, passwd);
+            pstmt = con.prepareStatement(sql);
+
+            if (conditions != null) {
+                int paramIndex = 1;
+                for (Entry<String, String> entry : conditions.entrySet()) {
+                    String value = entry.getValue();
+                    if (ValidateUtils.isBlank(value)) {
+                        continue;
+                    }
+
+                    pstmt.setString(paramIndex++, value);
+                }
+            }
+
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                // empVO 也稱為 Domain objects
+                empVO = new EmpVO();
+                empVO.setEmpno(rs.getInt("empno"));
+                empVO.setEname(rs.getString("ename"));
+                empVO.setJob(rs.getString("job"));
+                empVO.setHiredate(rs.getDate("hiredate"));
+                empVO.setSal(rs.getInt("sal"));
+                empVO.setComm(rs.getDouble("comm"));
+                empVO.setDeptno(rs.getInt("deptno"));
+                list.add(empVO); // Store the row in the list
+            }
+
+            // Handle any SQL errors
+        } catch (SQLException se) {
+            throw new RuntimeException("A database error occured. " + se.getMessage());
+            // Clean up JDBC resources
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException se) {
+                    se.printStackTrace(System.err);
+                }
+            }
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException se) {
+                    se.printStackTrace(System.err);
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (Exception e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+        }
+        return list;
+    }
+
+    private String transformSQL(Map<String, String> conditions) {
+        StringBuilder sql = new StringBuilder(GET_ALL_BASE_STMT);
+        if (conditions != null) {
+            for (Entry<String, String> entry : conditions.entrySet()) {
+                if (ValidateUtils.isBlank(entry.getValue())) {
+                    continue;
+                }
+                String condition = transformCondition(entry.getKey());
+                if (condition != null) {
+                    sql.append(" AND ").append(condition);
+                }
+            }
+        }
+        sql.append(" ORDER BY empno");
+        return sql.toString();
+    }
+
+    private String transformCondition(String columnName) {
+        /*
+         * TODO [進階] 思考 in、between、>=、<=、… 時該怎麼撰寫程式碼
+         * 提示：
+         *   1. ServletRequest.getParameterMap() 方法 回傳 java.util.Map<java.lang.String,java.lang.String[]>
+         *   2. 修改方法參數(如使用 enum、常數…等)，使程式可以判斷使用何種 where operator
+         */
+        switch (columnName) {
+            case "empno":
+            case "sal":
+            case "comm":
+            case "deptno":
+                return columnName + " = ?";
+            case "ename":
+            case "job":
+                return columnName + " like '%' || ? || '%'";
+            case "hiredate":
+                return columnName + " = to_date(?, 'yyyy-mm-dd')";
+            default:
+                return null;
+        }
     }
 
     @Override
